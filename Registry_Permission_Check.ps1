94ae643c83faa1d7c3f2c53a4b3de84a6e97caf1
Christophe Pelichet <c.pelichet@gmail.com>
2021-06-20 10:16:51 +0200

    type + update changelog
    type + update changelog

--- Registry_Permission_Check.ps1
+++ Registry_Permission_Check.ps1
@@ -15,16 +15,16 @@ Registry_Permission_Check.ps1 - Simple script ton check if a permission is appli
 Simple script ton check if a permission is applied and write a log if ok or not. Useful for debugging coupled with a schedule task to see when the permission changes.
 
 .OUTPUTS
-Write a log (configured by the variable $LOGFULLPATH) with the status (configured by the variable $REGPERMOK or $REGPERMKO).
+Write a log (configured by the variable $LOGFULLPATH) with the status (configured by the variable $regPermOk or $regPermKo).
 
 .CONFIGURATION
-Configure the registry key you want to check with the variable : $REGKEY
-Configure the account for which you want to check access on the registry key with the variable : $REGKEYPERM
-Configure the desired text in the log file for the result of the control with the variables : $REGPERMOK and $REGPERMKO
-Configure the log dir path and the log file name with the variables : $REGPERMLOGDIR and $REGPERMLOGFILE
-Configure the tmp dir and file with the variables : $TMPDIR and $TMPRIGHT
-Configure the log writed in the log file with th variabke : $DATE
-Configure the text for the result in the log file with the variables : $REGPERMOK $REGPERMKO
+Configure the registry key you want to check with the variable : $regkey
+Configure the account for which you want to check access on the registry key with the variable : $regkeyPerm
+Configure the desired text in the log file for the result of the control with the variables : $regPermOk and $regPermKo
+Configure the log dir path and the log file name with the variables : $regPermLogDir and $regPermLogFile
+Configure the tmp dir and file with the variables : $tmpdir and $tmpRight
+Configure the log writed in the log file with th variabke : $date
+Configure the text for the result in the log file with the variables : $regPermOk $regPermKo
 
 .LINK
 
@@ -36,8 +36,9 @@ Find me on:
 * LinkedIn:     https://linkedin.com/in/christophepelichet
 * Github:       https://github.com/ChristophePelichet
  
-Change Log 
-V1.00 - 09/29/2020 - Initial version 
+Change Log
+V1.01   -   06/20/2021  - Clean Code 
+V1.00   -   09/29/2020  - Initial version 
 #>
 
 
@@ -60,31 +61,31 @@ V1.00 - 09/29/2020 - Initial version
 #######################################################
 
 ## Registry Key Path 
-$REGKEY = ""
+$regkey = ""
 
 
 ## Registey Key Permission
-$REGKEYPERM = ""
+$regkeyPerm = ""
 
 ## Reset registry permission list 
-$REGPERMLIST = ""
+$regPemList = ""
 
 ## Log File Name 
-$REGPERMLOGDIR  = "C:\Temp"
-$REGPERMLOGFILE = "permission_registry_log.txt"
-$REGPERMLOGFUPA = "$REGPERMLOGDIR\$REGPERMLOGFILE"
+$regPermLogDir  = ""
+$regPermLogFile = ""
+$regPermLogPath = "$regPermLogDir\$regPermLogFile"
 
 ## Temp Directory 
-$TMPDIR ="C:\Temp"
-$TMPRIGHT = "permission_registry_temp.txt"
-$TMPRIGHTPATH = "$TMPDIR\$TMPRIGHT"
+$tmpdir =""
+$tmpRight = ""
+$tmpRightPath = "$tmpdir\$tmpRight"
 
 ## Date
-$DATE = (Get-Date -UFormat "%A %m/%d/%Y %R")
+$date = (Get-Date -UFormat "%A %m/%d/%Y %R")
 
 # Log Text
-$REGPERMOK = "Permission OK"
-$REGPERMKO = "Permission KO"
+$regPermOk = "Permission OK"
+$regPermKo = "Permission KO"
 
 
 #######################################################
@@ -92,29 +93,29 @@ $REGPERMKO = "Permission KO"
 #######################################################
 
 ## Clean temp right file before loop
-if ( test-path $TMPRIGHTPATH ) { Remove-Item -Path $TMPRIGHTPATH }
+if ( test-path $tmpRightPath ) { Remove-Item -Path $tmpRightPath }
 
 
 ## Get Registry Permission
-(get-acl -Path $REGKEY).AccessToString | Format-List Owner | Out-File -FilePath  $TMPRIGHTPATH
-$REGPERMLIST = Get-Content -Path $TMPRIGHTPATH
+(get-acl -Path $regkey).AccessToString | Format-List Owner | Out-File -FilePath  $tmpRightPath
+$regPemList = Get-Content -Path $tmpRightPath
 
 ## Reset Loop Counter 
-$REGCOUNT = 0
+$regCount = 0
 
 ## Check if permission is system
-foreach ( $REGPERMRESULT in $REGPERMLIST ) {
-    if ( $REGPERMRESULT -match "$REGKEYPERM" ) {
-        $REGCOUNT++
+foreach ( $regPermResult in $regPemList ) {
+    if ( $regPermResult -match "$regkeyPerm" ) {
+        $regCount++
     }
 }
 
-if ( $REGCOUNT -eq "1") {
-    Write-Output "$DATE : $REGPERMOK" | Out-file -FilePath $REGPERMLOGFUPA -Append
+if ( $regCount -eq "1") {
+    Write-Output "$date : $regPermOk" | Out-file -FilePath $regPermLogPath -Append
     } else {
-    Write-Output "$DATE : $REGPERMKO" | Out-file -FilePath $REGPERMLOGFUPA -Append
+    Write-Output "$date : $regPermKo" | Out-file -FilePath $regPermLogPath -Append
     }
 
 
 ## Clean temp right file after loop
-Remove-Item -Path $TMPRIGHTPATH
+Remove-Item -Path $tmpRightPath
